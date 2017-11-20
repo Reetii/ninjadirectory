c44a39e86a47b9912ad7db6ffc0dcabd106994b3
Reetii <reetigarg12@gmail.com>
2017-11-20 00:15:06 -0800

    Added th service and the filterpipe files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { LoggingService } from './logging.service';
+import { DataService } from './data.service';
+
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DirectoryComponent } from './directory/directory.component';
+import { routing } from './app.routes';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DirectoryComponent,
+    FilterPipe
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [LoggingService, DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { LoggingService } from '../logging.service';
+
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  
+})
+export class HomeComponent implements OnInit {
+
+
+
+  constructor(private logger: LoggingService) {}
+    logIt(){
+    this.logger.log();
+    }
+
+
+
+
+  ngOnInit() {
+  }
+
+}
